1944d74efa692031c60cc26b10b3bad8370c8188
Matthew van Bommel <matthew.vbommel@gmail.com>
2020-03-14 18:18:55 -0700

    Commas working in queries

--- global.R
+++ global.R
@@ -1,35 +1,38 @@
 `%>%` = dplyr::`%>%`
 
 # TO DO ----
-# * allow user to add inputs (use info in data_information$columns to determine input type)
 # * marker information - let user select columns to display?
 # * search by drawing box on map
 # * fix HTTP error on load
 # * loading animation
 # * add data view
 # * add data export
+# * Error handling (especially in query)
 
 # Next Step:
-# Use filter inputs to actually filter query
 
 # DATA ----
+# * Data Sources ----
+dataset_names = c("City Maintained Trees",
+                  "Dispatch Data")
+
+datasets = c("https://services5.arcgis.com/54falWtcpty3V47Z/arcgis/rest/services/City_Maintained_Trees/FeatureServer/0",
+             "https://services5.arcgis.com/54falWtcpty3V47Z/arcgis/rest/services/cad_calls_year3/FeatureServer/0")
+
+names(datasets) = dataset_names
+
 # Initialize data_information for load (will update with observeEvent)
 data_information = NULL
 
+range_variable_types = c("smallinteger", "integer", "single", "double", "oid", "date")
+
 # FUNCTIONS ----
+# * Utility ----
 epoch_to_calendar_date = function(epoch) {
   as.Date(as.POSIXct(epoch / 1000, 
                      origin = '1970-01-01',
                      tz = 'America/Los_Angeles'),
           format = '%y-%m-%d')
-}
-
-get_data_information = function(url) {
-  json = jsonlite::fromJSON(txt = paste0(url, "/?f=pjson"))
-  
-  information = list(name = json$name, 
-                     max_record_count = json$maxRecordCount,
-                     columns = json$fields)
 }
 
 geom_to_longitude_latitude = function(data) {
@@ -42,17 +45,34 @@ geom_to_longitude_latitude = function(data) {
     # Missing Latitude and Longitude to NA
     coordinates$longitude[coordinates$longitude < -142] = NA
     coordinates$latitude[coordinates$latitude < 32] = NA
-      
+    
     data = cbind(data, coordinates) %>%
       dplyr::select(-geoms)
   }
   
   data
+  
+}
 
+# * Data ----
+get_data_information = function(url) {
+  json = jsonlite::fromJSON(txt = paste0(url, "/?f=pjson"))
+  
+  information = list(name = json$name, 
+                     max_record_count = json$maxRecordCount,
+                     columns = json$fields)
+}
+
+# * Filter ----
+get_variable_type = function(variable_name, column_information) {
+  variable_type = column_information %>%
+    dplyr::filter(variable_name == name) %>%
+    dplyr::mutate(type = tolower(stringr::str_replace(string = type, pattern = "esriFieldType", replacement = ""))) %>%
+    dplyr::pull(type)
 }
 
 get_filter_values = function(url, variable, variable_type) {
-  if (variable_type %in% c("smallinteger", "integer", "single", "double", "oid", "date")) {
+  if (variable_type %in% range_variable_types) {
     min_value = jsonlite::fromJSON(paste0(url, "/query?where=", variable, "%20IS%20NOT%20NULL&outFields=", variable, "&orderByFields=", variable, "&returnGeometry=false&resultRecordCount=1&outSR=4326&f=json"))$features$attributes[, 1]
     max_value = jsonlite::fromJSON(paste0(url, "/query?where=", variable, "%20IS%20NOT%20NULL&outFields=", variable, "&orderByFields=", variable, "%20DESC&returnGeometry=false&resultRecordCount=1&outSR=4326&f=json"))$features$attributes[, 1]
     
@@ -63,17 +83,16 @@ get_filter_values = function(url, variable, variable_type) {
     
     values = c(min_value, max_value)
   } else {
-    values = jsonlite::fromJSON(paste0(url, "/query?where=outFields=", variable, "&returnGeometry=false&returnDistinctValues=true&outSR=4326&f=json"))$features$attributes[, 1]
+    values = jsonlite::fromJSON(paste0(url, "/query?where=1=1&outFields=", variable, "&returnGeometry=false&returnDistinctValues=true&outSR=4326&f=json"))$features$attributes[, 1]
   }
   
   return(values)
 }
 
 create_filter_input = function(filter_index, filters, url, column_information) {
-  variable_type = column_information %>%
-    dplyr::filter(name == filters[filter_index]) %>%
-    dplyr::mutate(type = tolower(stringr::str_replace(string = type, pattern = "esriFieldType", replacement = ""))) %>%
-    dplyr::pull(type)
+
+  variable_type = get_variable_type(variable_name = filters[filter_index],
+                                    column_information = column_information)
   
   filter_input_id = paste0("filter_", filter_index)
   filter_label = h3(filters[filter_index])
@@ -104,13 +123,6 @@ create_filter_input = function(filter_index, filters, url, column_information) {
 
 
 
-# URL ----
-dataset_names = c("City Maintained Trees",
-                  "Dispatch Data")
 
-datasets = c("https://services5.arcgis.com/54falWtcpty3V47Z/arcgis/rest/services/City_Maintained_Trees/FeatureServer/0",
-             "https://services5.arcgis.com/54falWtcpty3V47Z/arcgis/rest/services/cad_calls_year3/FeatureServer/0")
-
-names(datasets) = dataset_names
          
 
--- server.R
+++ server.R
@@ -16,7 +16,11 @@ server = function(input, output, session) {
   
   # Initialize Reactive Values ----
   reactive_values = shiny::reactiveValues()
+  reactive_values$new_query_count = 0
+  reactive_values$previous_query = ""
+
   
+  # Dataset ----
   url = reactive({
     input$dataset_picker
   })
@@ -25,6 +29,7 @@ server = function(input, output, session) {
     get_data_information(url = url())
   })
   
+  # Filters ----
   observeEvent(input$dataset_picker, {
     shinyWidgets::updatePickerInput(
       session = session,
@@ -37,7 +42,9 @@ server = function(input, output, session) {
     filters = input$filter_picker
     number_of_filters = length(filters)
     
-    if (number_of_filters > 0) {
+    column_information = data_information()$columns
+  
+    if (number_of_filters > 0 & all(filters %in% column_information$name)) {
       lapply(1:number_of_filters, function(filter_index) {
         create_filter_input(filter_index = filter_index, 
                             filters = filters, 
@@ -49,14 +56,104 @@ server = function(input, output, session) {
     }
   })
   
+  # Data ----
+  # * Create Query ----
+  query_filter = reactive({
+    filter_query = "1=1"
+   
+    filters = input$filter_picker
+    number_of_filters = length(filters)
+    
+    column_information = data_information()$columns
+   
+    if (!is.null(filters)) {
+      if (all(filters %in% column_information$name)) {
+        for (filter_index in 1:number_of_filters) {
+          variable_name = filters[filter_index]
+          variable_type = get_variable_type(variable_name = variable_name,
+                                            column_information = column_information)
+  
+          filter_values = input[[paste0("filter_", filter_index)]]
+  
+          if (!is.null(filter_values)) {
+            if (variable_type %in% range_variable_types) {
+              if (variable_type == "date") {
+                lower_filter = paste0(" date '", filter_values[1], "' ")
+                upper_filter =  paste0(" date '", filter_values[2], "' ")
+              } else {
+                lower_filter = filter_values[1]
+                upper_filter = filter_values[2]
+              }
+              filter_query = paste0(filter_query, paste0(" AND ", variable_name, " >= ", lower_filter,
+                                                         " AND ", variable_name, " <= ", upper_filter))
+            } else {
+              filter_query = paste0(filter_query, " AND (", variable_name, " = '", paste0(filter_values,
+                                                                                          collapse = paste0("' OR ", variable_name, " = '")),
+                                                                                          "') ")
+            }
+          }
+        }
+      }
+      
+      # if (length(input$dispatch_map_draw_new_feature) > 0) {
+      #   
+      #   boundaries = as.data.frame(matrix(unlist(input$dispatch_map_draw_new_feature$geometry$coordinates),
+      #                                     ncol = 2, 
+      #                                     byrow = TRUE),
+      #                              stringsAsFactors = FALSE)
+      #   colnames(boundaries) = c('longitude', 'latitude')
+      #   
+      #   min_longitude = min(boundaries$longitude)
+      #   max_longitude = max(boundaries$longitude)
+      #   min_latitude = min(boundaries$latitude)
+      #   max_latitude = max(boundaries$latitude)
+      #   
+      #   # Convert longitude and latitude to X and Y for query
+      #   min_X_Coordinate = predict(longitude_model, newdata = data.frame(longitude = min_longitude))
+      #   max_X_Coordinate = predict(longitude_model, newdata = data.frame(longitude = max_longitude))
+      #   min_Y_Coordinate = predict(latitude_model, newdata = data.frame(latitude = min_latitude))
+      #   max_Y_Coordinate = predict(latitude_model, newdata = data.frame(latitude = max_latitude))
+      #   
+      #   shape_filter = paste0(" AND X_Coordinate >= ", min_X_Coordinate,
+      #                         " AND X_Coordinate <= ", max_X_Coordinate,
+      #                         " AND Y_Coordinate >= ", min_Y_Coordinate,
+      #                         " AND Y_Coordinate <= ", max_Y_Coordinate,
+      #                         " ")
+      #   
+      #   query = paste0(query, shape_filter)
+      #   
+      # }
+    }
+    
+    return(filter_query)
+  })
+  
+  # Check to see if query has changed, and if so, add 1 to new_query_count reactive value
+  observeEvent(query(), {
+    if (!is.null(query()) && query() != reactive_values$previous_query) {
+      reactive_values$new_query_count = reactive_values$new_query_count + 1
+    }
+    reactive_values$previous_query = query()
+  })
+  
+  query = reactive({
+    paste0(url(), "/where=", query_filter())
+  })
+  
+  # Only update data if new query
+  filtered_data = eventReactive(reactive_values$new_query_count, {
+    data = esri2sf::esri2sf(url = url(), 
+                            where = query_filter(),
+                            #limit = data_information()$max_record_count) %>%
+                            limit = 10) %>%
+      as.data.frame() %>%
+      geom_to_longitude_latitude()
+  })
+  
   # * Map Output ----
   output$map = leaflet::renderLeaflet({
  
-    data = esri2sf::esri2sf(url = url(), 
-                            where = "1=1",
-                            limit = data_information()$max_record_count) %>%
-      as.data.frame() %>%
-      geom_to_longitude_latitude() %>%
+    data = filtered_data() %>%
       # Need to filter NAs to avoid javascript error
       filter(!is.na(latitude) & !is.na(longitude))
     
